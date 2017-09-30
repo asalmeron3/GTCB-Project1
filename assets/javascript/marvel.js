f4a2b4fe85225a2596684620b631f4c3667d8c1e
Lina C <linaclark96@gmail.com>
2017-09-30 12:52:58 -0400

    updated code

--- assets/javascript/marvel.js
+++ assets/javascript/marvel.js
@@ -18,20 +18,20 @@ firebase.initializeApp(config);
 $(document).ready(function() {
 
     var createDiv = $('<div class= "character">');
-    var queryURL = "https://gateway.marvel.com:443/v1/public/characters?name=Misty%20Knight&ts=" + ts + "&apikey=" + apiKey + "&hash=" + passhash + "&limit=5";
+    var queryURL = "https://gateway.marvel.com:443/v1/public/characters?name=Spider-Man&ts=" + ts + "&apikey=" + apiKey + "&hash=" + passhash + "&limit=5";
     $.ajax({
         url: queryURL,
         method: "GET"
     }).done(function(response) {
-        //console.log(response);
+        console.log(response);
         var name = response.data.results["0"].name;
         var thumbnail = response.data.results["0"].thumbnail.path;
-        var stories = response.data.results["0"].stories.collectionURI;
+        var story = response.data.results["0"].description;
         var comic = response.data.results["0"].comics.items["0"].name;
         var characterId = response.data.results["0"].id;
         var p = $("<p>");
         p.append(name);
-        p.append(stories);
+        p.append(story);
         var img = $("<img>").attr("src", thumbnail + ".jpg");
         p.append(img);
         createDiv.append(p);
@@ -43,6 +43,7 @@ $(document).ready(function() {
         }).done(function(secondResponse) {
             console.log(secondResponse);
             var firstComicImage = secondResponse.data.results['0'].thumbnail.path;
+            console.log(firstComicImage);
             var firstComicTitle = secondResponse.data.results['0'].title;
             var secondComicImage = secondResponse.data.results['1'].thumbnail.path;
             var secondComicTitle = secondResponse.data.results['1'].title;
@@ -52,12 +53,20 @@ $(document).ready(function() {
             var fourthComicTitle = secondResponse.data.results['3'].title;
 
             var innerDiv = $("<div>");
-            var innerImg = $("<img>").attr("src", firstComicImage + ".jpg");
             var pTwo = $("<p>");
+            for (var x = 0; x < 4; x++) {
+                var comicImage = secondResponse.data.results[x].thumbnail.path;
+                console.log(comicImage);
+                var comicTitle = secondResponse.data.results[x].title;
+                var innerImg = $("<img>").attr("src", comicImage + ".jpg");
+                innerDiv.append(innerImg);
+                p.append(comicTitle);
+                innerDiv.append(p);
+            }
+
             createDiv.append(innerDiv);
-            innerDiv.append(innerImg);
-            p.append(firstComicTitle);
-            innerDiv.append(p);
+
+
 
             $("body").append(createDiv);
 
